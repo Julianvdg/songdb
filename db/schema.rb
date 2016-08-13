86fb530ba7f8cc3b24d60e5793fe9e99c0019c3d
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-08-13 11:37:46 +0200

    Added relationship between artist and song. Created the artist_id column in the songs table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813092515) do
+ActiveRecord::Schema.define(version: 20160813093501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20160813092515) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
+  add_foreign_key "songs", "artists"
 end
