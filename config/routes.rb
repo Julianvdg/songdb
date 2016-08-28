8257727e4b51f5c54c7eccfa63a3e9a6121b9937
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-08-28 11:26:01 +0200

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'artists/index'
+  get 'artists' => 'artists#show'
 
-  get 'artists/show'
+  get 'artists/:id' => 'artists#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
